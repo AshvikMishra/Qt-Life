acad010a63ffed8aad546880c7a52c90345bd8db
Ashvik Mishra <ashvikmishra.mit@gmail.com>
2024-09-22 02:51:53 +0530

    Chnages to ML ouput page

--- application/productivity_app/lib/pages/ml_ouput.dart
+++ application/productivity_app/lib/pages/ml_ouput.dart
@@ -8,11 +8,24 @@ class MLOutput extends StatefulWidget {
 }
 
 class _MLOutputState extends State<MLOutput> {
+  // Assuming you will add your TFLite model logic here
+  String mlOutput = "Output from productivity_model.tflite";
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text(
-        "Machine Learning Output page"
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Card(
+          color: Colors.grey[900],
+          child: Center(
+            child: Text(
+              mlOutput,
+              style: TextStyle(fontSize: 20, color: Colors.grey[400]),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
       ),
     );
   }
